9ba6a79bdc9c2ceb8fa5f9a272f4945c2aedcc8d
Efthymios Hadjimichael <ehadjimichael@id5.io>
2023-12-02 15:53:16 +0200

    Day 1 part 2

--- src/advent/d01.rs
+++ src/advent/d01.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use super::util;
 
 pub fn run(pt: u8) -> Result<(), Box<dyn std::error::Error>> {
@@ -8,8 +9,10 @@ pub fn run(pt: u8) -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
+const DATAFILE: &str = "./data/d01pt1.txt";
+
 fn pt1() -> Result<(), Box<dyn std::error::Error>> {
-    let lines = util::parse_in_lines("./data/ex01pt1.txt")?;
+    let lines = util::parse_in_lines(DATAFILE)?;
     let mut numbers = Vec::new();
     for line in lines {
         let string: String = line.parse()?;
@@ -34,5 +37,32 @@ fn pt1() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn pt2() -> Result<(), Box<dyn std::error::Error>> {
-    todo!()
+    let lines = util::parse_in_lines(DATAFILE)?;
+    let mut numbers = Vec::new();
+    let numstrings: HashMap<&str, u32> = HashMap::from([
+        ("one",1), ("two",2), ("three",3), ("four",4), ("five",5), ("six",6), ("seven",7), ("eight",8), ("nine",9),
+        ("1",1), ("2",2), ("3",3), ("4",4), ("5",5), ("6",6), ("7",7), ("8",8), ("9",9),
+    ]);
+    for line in lines {
+        let string: String = line.parse()?;
+        let mut first: Option<u32> = Option::None;
+        let mut last: Option<u32> = Option::None;
+        for i in 0..string.len() {
+            let t = string.get(i..).unwrap();
+            numstrings.iter().for_each(|(&s,&i)| {
+                if t.starts_with(s) {
+                    last = Some(i);
+                    if first == Option::None {
+                        first = last;
+                    }
+                }
+            });
+        }
+        if let (Some(f),Some(l)) = (first,last) {
+            println!("line: {} -- {}{}", line, f, l);
+            numbers.push(f * 10 + l);
+        }
+    }
+    println!("{}", numbers.iter().sum::<u32>());
+    Ok(())
 }
