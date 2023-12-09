cfc249cccc1e3e9a83cffba417f2c8f0fd11e1a3
Efthymios Hadjimichael <ehadjimichael@id5.io>
2023-12-09 09:55:17 +0200

    Day 8 pt2

--- src/advent/d08.rs
+++ src/advent/d08.rs
@@ -5,6 +5,8 @@ use nom::sequence::tuple;
 use nom::IResult;
 use std::collections::HashMap;
 
+use crate::advent::util::lcm;
+
 struct Graph<'a> {
     map: HashMap<&'a str, (&'a str, &'a str)>,
 }
@@ -16,12 +18,13 @@ fn parse_directions(input: &str) -> IResult<&str, String> {
 }
 
 fn parse_graph(input: &str) -> IResult<&str, Graph> {
-    let (rem, v) = separated_list1(tag("\n"), tuple((alpha1, tag(" = ("), alpha1, tag(", "), alpha1, tag(")"))))(input)?;
-    let init = v.first().unwrap().0;
-    let map: HashMap<&str, (&str, &str)> = v.iter().map(|(s, _, l, _, r, _)| (*s, (*l, *r))).collect();
-    Ok((rem, Graph {
-        map,
-    }))
+    let (rem, v) = separated_list1(
+        tag("\n"),
+        tuple((alpha1, tag(" = ("), alpha1, tag(", "), alpha1, tag(")"))),
+    )(input)?;
+    let map: HashMap<&str, (&str, &str)> =
+        v.iter().map(|(s, _, l, _, r, _)| (*s, (*l, *r))).collect();
+    Ok((rem, Graph { map }))
 }
 
 pub fn pt1(path: String) -> Result<(), Box<dyn std::error::Error>> {
@@ -72,28 +75,48 @@ pub fn pt2(path: String) -> Result<(), Box<dyn std::error::Error>> {
         )));
     }
 
-    let mut posv: Vec<&str> = graph.map.keys().filter(|s| s.ends_with('A')).map(|s| *s).collect();
-    let mut moves = 0;
-    for (idx, c) in directions.chars().cycle().enumerate() {
-        println!("{:?}", posv);
-        if posv.iter().filter(|s| s.ends_with('Z')).count() == posv.len() {
-            moves = idx;
-            break;
-        }
-        let mut next: Vec<&str> = Vec::new();
-        for &pos in &posv {
+    let posv: Vec<&str> = graph
+        .map
+        .keys()
+        .filter(|s| s.ends_with('A'))
+        .map(|s| *s)
+        .collect();
+    let mut cadences: Vec<usize> = Vec::new();
+    let mut offsets_to_z: Vec<usize> = Vec::new();
+    for init in &posv {
+        let mut pos = *init;
+        let mut moves_to_z = Vec::new();
+        for (idx, c) in directions.chars().cycle().enumerate() {
+            if pos.ends_with("Z") {
+                moves_to_z.push(idx);
+                if moves_to_z.len() == 3 {
+                    let first = *moves_to_z.first().unwrap();
+                    let solvable =
+                        moves_to_z.iter().fold(
+                            true,
+                            |state, &x| if !state { false } else { x % first == 0 },
+                        );
+                    if !solvable {
+                        panic!("Problem not solvable with least-common-multiple");
+                    }
+                    offsets_to_z.push(first);
+                    break;
+                }
+            }
             if let Some((l, r)) = graph.map.get(pos) {
                 match c {
-                    'L' => next.push(l),
-                    'R' => next.push(r),
+                    'L' => pos = l,
+                    'R' => pos = r,
                     _ => unreachable!(),
                 };
             } else {
                 unreachable!();
             }
         }
-        posv = next;
     }
-    println!("Moves to __Z: {}", moves);
+    println!("Moves to __Z: {:?}", offsets_to_z);
+    let lcm = offsets_to_z.iter().fold(
+        *(offsets_to_z.first().unwrap()) as i64, |s, &x| lcm(s, x as i64));
+    println!("Least Common Multiple: {}", lcm);
     Ok(())
 }
--- src/advent/util.rs
+++ src/advent/util.rs
@@ -31,3 +31,48 @@ where
         println!();
     }
 }
+
+#[allow(dead_code)]
+pub fn gcd(u: i64, v: i64) -> i64 {
+    // `wrapping_abs` gives a number's absolute value, unless that's 2³¹. 2³¹
+    // won't fit in `i64`, so it gives -2³¹ instead.
+    let mut v = v.wrapping_abs() as u64;
+    if u == 0 {
+        return v as i64;
+    }
+    let mut u = u.wrapping_abs() as u64;
+    if v == 0 {
+        return u as i64;
+    }
+
+    // `|` is bitwise OR. `trailing_zeros` quickly counts a binary number's
+    // trailing zeros, giving its prime factorization's exponent on two.
+    let gcd_exponent_on_two = (u | v).trailing_zeros();
+
+    // `>>=` divides the left by two to the power of the right, storing that in
+    // the left variable. `u` divided by its prime factorization's power of two
+    // turns it odd.
+    u >>= u.trailing_zeros();
+    v >>= v.trailing_zeros();
+
+    while u != v {
+        if u < v {
+            // Swap the variables' values with each other.
+            core::mem::swap(&mut u, &mut v);
+        }
+        u -= v;
+        u >>= u.trailing_zeros();
+    }
+
+    // `<<` multiplies the left by two to the power of the right.
+    (u << gcd_exponent_on_two) as i64
+}
+
+#[allow(dead_code)]
+pub fn lcm(u: i64, v: i64) -> i64 {
+    if u > v {
+        (u / gcd(u, v)) * v
+    } else {
+        (v / gcd(v, u)) * u
+    }
+}
