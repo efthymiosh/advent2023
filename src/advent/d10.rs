fd2bbf3882dd85542a9257e1181238993d01987e
Efthymios Hadjimichael <ehadjimichael@id5.io>
2023-12-16 09:10:33 +0200

    Day 10

--- src/advent/d10.rs
+++ src/advent/d10.rs
@@ -2,9 +2,6 @@ use super::util;
 
 fn get_valid_neighbors(grid: &[&mut [char]], (x,y): (usize, usize)) -> Vec<(usize,usize)> {
     let mut valid_paths = Vec::new();
-    println!("  {}  ", grid[x][y - 1]);
-    println!("{} S {}", grid[x -1][y], grid[x + 1][y]);
-    println!("  {}  ", grid[x][y + 1]);
     match grid[x - 1][y] {
         '-' | 'L' | 'F' => { valid_paths.push((x - 1, y)); },
         _ => {},
@@ -63,6 +60,62 @@ fn discover_weights(grid: &Vec<&mut [char]>, weights: &mut Vec<&mut [u64]>, (x, 
     }
 }
 
+fn discover_loop(grid: &Vec<&mut [char]>, mark: &mut Vec<&mut [u64]>, vertices: &mut Vec<(u64, u64)>, (x, y): (usize, usize)) {
+    if mark[x][y] == 1 {
+        return;
+    }
+    mark[x][y] = 1;
+    vertices.push((x as u64, y as u64));
+    match grid[x][y] {
+        '|' => {
+            discover_loop(grid, mark, vertices, (x, y - 1));
+            discover_loop(grid, mark, vertices, (x, y + 1));
+        }
+        '-' => {
+            discover_loop(grid, mark, vertices, (x + 1, y));
+            discover_loop(grid, mark, vertices, (x - 1, y));
+        }
+        'L' => {
+            discover_loop(grid, mark, vertices, (x + 1, y));
+            discover_loop(grid, mark, vertices, (x, y - 1));
+        }
+        'J' => {
+            discover_loop(grid, mark, vertices, (x - 1, y));
+            discover_loop(grid, mark, vertices, (x, y - 1));
+        }
+        '7' => {
+            discover_loop(grid, mark, vertices, (x - 1, y));
+            discover_loop(grid, mark, vertices, (x, y + 1));
+        }
+        'F' => {
+            discover_loop(grid, mark, vertices, (x + 1, y));
+            discover_loop(grid, mark, vertices, (x, y + 1));
+        }
+        _ => {
+            panic!("Reached unexpected {}", grid[x][y]);
+        }
+    }
+}
+
+fn polygon_area(vertices: &[(u64, u64)]) -> f64 {
+    let n = vertices.len();
+    let mut sum = 0.0;
+
+    for cur in 0..n {
+        let next = (cur + 1) % n;
+        sum += (vertices[cur].0 as f64 * vertices[next].1 as f64) - (vertices[next].0 as f64 * vertices[cur].1 as f64);
+    }
+
+    0.5 * sum.abs()
+}
+
+fn picks_theorem(area: f64, vertices: &[(u64, u64)]) -> u64 {
+    let boundary_points = vertices.len() as u64;
+    let interior_points = area - (boundary_points / 2) as f64 + 1.0;
+
+    interior_points as u64
+}
+
 pub fn pt1(path: String) -> Result<(), Box<dyn std::error::Error>> {
     let mut lines = util::parse_in_lines(&path)?.peekable();
 
@@ -102,6 +155,38 @@ pub fn pt1(path: String) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-pub fn pt2(_path: String) -> Result<(), Box<dyn std::error::Error>> {
+pub fn pt2(path: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut lines = util::parse_in_lines(&path)?.peekable();
+
+    let size = lines.peek().ok_or("Bad input file")?.len();
+
+    let mut grid_raw = vec![' '; size * size];
+    let mut grid: Vec<&mut [char]> = grid_raw.as_mut_slice().chunks_mut(size).collect();
+    let mut mark_raw = vec![0; size * size];
+    let mut mark: Vec<&mut [u64]> = mark_raw.as_mut_slice().chunks_mut(size).collect();
+
+    let (mut startx, mut starty) = (0,0);
+    for (i, line) in lines.enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            grid[j][i] = c;
+            if c == 'S' {
+                (startx, starty) = (j, i);
+            }
+        }
+    }
+    mark[startx][starty] = 1;
+    let v = get_valid_neighbors(&grid, (startx, starty));
+    let mut vertices: Vec<(u64, u64)> = Vec::new();
+    vertices.push((startx as u64, starty as u64));
+    for (x,y) in v {
+        discover_loop(&grid, &mut mark, &mut vertices, (x, y));
+    }
+
+    let area = polygon_area(vertices.as_slice());
+    println!("Area: {}\nPolygon Count: {}", area, vertices.len());
+    let inside_tiles: u64 = picks_theorem(area, vertices.as_slice());
+
+    println!("Inside tiles: {:?}", inside_tiles);
+
     Ok(())
 }
