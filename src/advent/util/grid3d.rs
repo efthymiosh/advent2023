2f9dcc424a2ef8db76210b49079e198ec92a1734
Efthymios Hadjimichael <ehadjimichael@id5.io>
2023-12-28 14:47:48 +0200

    Refactor 3d grid vis

--- src/advent/util/grid3d.rs
+++ src/advent/util/grid3d.rs
@@ -1,27 +1,33 @@
 use std::f32::consts::PI;
 
-use bevy::prelude::*;
 use bevy::pbr::CascadeShadowConfigBuilder;
+use bevy::prelude::*;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 use num_traits::PrimInt;
 use std::fmt::Debug;
 
 #[derive(Resource)]
-struct Plane<T> where T: PrimInt+Debug {
+struct Plane<T>
+where
+    T: PrimInt + Debug + Sync + Send + 'static,
+{
     grid: Vec<Vec<Vec<T>>>,
 }
 
-
-impl<T: PrimInt+Debug+Sync+Send + 'static> Plane<T> {
+impl<T: PrimInt + Debug + Sync + Send + 'static> Plane<T> {
     fn setup(
         mut commands: Commands,
         mut meshes: ResMut<Assets<Mesh>>,
         mut materials: ResMut<Assets<StandardMaterial>>,
+        mut ambient_light: ResMut<AmbientLight>,
         plane: Res<Plane<T>>,
     ) {
+        ambient_light.color = Color::WHITE;
+        ambient_light.brightness = 0.7;
         commands.spawn(DirectionalLightBundle {
             directional_light: DirectionalLight {
                 shadows_enabled: true,
+                illuminance: 55000.0,
                 ..default()
             },
             transform: Transform {
@@ -29,15 +35,12 @@ impl<T: PrimInt+Debug+Sync+Send + 'static> Plane<T> {
                 rotation: Quat::from_rotation_x(-PI / 4.),
                 ..default()
             },
-            // The default cascade config is designed to handle large scenes.
-            // As this example has a much smaller world, we can tighten the shadow
-            // bounds for better visual quality.
             cascade_shadow_config: CascadeShadowConfigBuilder {
                 first_cascade_far_bound: 4.0,
                 maximum_distance: 10.0,
                 ..default()
             }
-                .into(),
+            .into(),
             ..default()
         });
 
@@ -45,7 +48,9 @@ impl<T: PrimInt+Debug+Sync+Send + 'static> Plane<T> {
         commands.spawn(PbrBundle {
             mesh: meshes.add(shape::Circle::new(50.0).into()),
             material: materials.add(Color::YELLOW_GREEN.into()),
-            transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+            transform: Transform::from_rotation(Quat::from_rotation_x(
+                -std::f32::consts::FRAC_PI_2,
+            )),
             ..default()
         });
         // cube
@@ -63,7 +68,7 @@ impl<T: PrimInt+Debug+Sync+Send + 'static> Plane<T> {
                                 ((plane.grid[x][y][z].to_u64().unwrap() * 5) % 256) as u8,
                                 ((plane.grid[x][y][z].to_u64().unwrap() * 3) % 256) as u8,
                             )
-                                .into(),
+                            .into(),
                         ),
                         transform: Transform::from_xyz(x as f32, z as f32, y as f32),
                         ..default()
@@ -74,7 +79,7 @@ impl<T: PrimInt+Debug+Sync+Send + 'static> Plane<T> {
         // camera
         commands.spawn((
             Camera3dBundle {
-                transform: Transform::from_translation(Vec3::new(0.0, 1.5, 5.0)),
+                transform: Transform::from_translation(Vec3::new(0.0, 150.0, 150.0)),
                 ..default()
             },
             PanOrbitCamera::default(),
@@ -82,13 +87,12 @@ impl<T: PrimInt+Debug+Sync+Send + 'static> Plane<T> {
     }
 }
 
-pub fn visualize<T: PrimInt+Debug+Sync+Send + 'static>(grid: &Vec<Vec<Vec<T>>>) {
-    let mut plane = Plane{grid: grid.clone()};
+pub fn visualize<T: PrimInt + Debug + Sync + Send + 'static>(grid: &Vec<Vec<Vec<T>>>) {
+    let plane = Plane { grid: grid.clone() };
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(PanOrbitCameraPlugin)
         .insert_resource(plane)
         .add_systems(Startup, Plane::<usize>::setup)
         .run();
-
 }
