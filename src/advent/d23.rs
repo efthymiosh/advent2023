fd754202051484f2b89a8724411abae9b2fb93a4
Efthymios Hadjimichael <ehadjimichael@id5.io>
2023-12-30 16:13:46 +0200

    Day 23 pt1

--- src/advent/d23.rs
+++ src/advent/d23.rs
@@ -1,8 +1,9 @@
-use std::collections::{HashSet, BinaryHeap};
+use std::collections::{HashSet, HashMap, VecDeque};
+use std::hash::Hash;
 
 use super::util;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct State {
     cost: u32,
     x: i64,
@@ -24,53 +25,70 @@ impl Ord for State {
     }
 }
 
-fn longest_path(grid: &mut [&mut [char]]) -> u32 {
+impl Hash for State {
+    // Omit Cost
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+        self.dx.hash(state);
+        self.dy.hash(state);
+    }
+}
+
+fn fin_path(grid: &mut [&mut [char]], queue: &mut VecDeque<State>, visited: &mut HashSet<State>, dist_grid: &mut Vec<Vec<u32>>) -> u32 {
     let size = grid.len();
-    let mut heap = BinaryHeap::new();
-    let mut visited = HashSet::new();
-    heap.push(State {x:0, y:1, dx:1, dy:0, cost: 0, path_id: 0});
-    heap.push(State {x:0, y:1, dx:0, dy:1, cost: 0, path_id: 0});
-
-    let mut dist_grid: Vec<Vec<u32>> = vec![vec![0; size]; size];
-
-    let mut max_distance = 0;
-    while let Some(state) = heap.pop() {
+    let mut max_distances: HashMap<u64, u32> = HashMap::new();
+    while let Some(state) = queue.pop_front() {
         if (state.x, state.y) == (size as i64 - 1, size as i64 - 2) {
-            max_distance = state.cost;
-            continue;
+            max_distances.insert(state.path_id, state.cost);
+            //break;
         }
         let mut neighbors = Vec::new();
         let x = state.x + state.dx;
         let y = state.y + state.dy;
-        if x < 0 || x >= size as i64 {
-            continue;
-        }
-        if y < 0 || y >= size as i64 {
-            continue;
-        }
-        match grid[x as usize][y as usize] {
-            '#' => continue,
-            '<' if state.dy == 1 => continue,
-            '>' if state.dy == -1 => continue,
-            '^' if state.dx == 1 => continue,
-            'v' if state.dx == -1 => continue,
-            _ => {},
-        }
         let cost = state.cost + 1;
-        neighbors.push(State { cost, x, y, dx: state.dy, dy: -state.dx, path_id: state.path_id});
-        neighbors.push(State { cost, x, y, dx: -state.dy, dy: state.dx, path_id: state.path_id});
-        neighbors.push(State { cost, x, y, dx: state.dx, dy: state.dy, path_id: state.path_id});
-        for nstate in neighbors {
-            if visited.contains(&(x, y, nstate.dx, nstate.dy)) {
+        let mut dpath = 0;
+        for (dx, dy) in [(state.dy, -state.dx), (-state.dy, state.dx), (state.dx, state.dy)] {
+            if x + dx < 0 || x + dx >= size as i64 {
                 continue;
             }
-            visited.insert((x, y, nstate.dx, nstate.dy));
-            heap.push(nstate);
+            if y + dy < 0 || y + dy >= size as i64 {
+                continue;
+            }
+            match grid[x as usize][y as usize] {
+                '#' => continue,
+                '<' if state.dy == 1 => continue,
+                '>' if state.dy == -1 => continue,
+                '^' if state.dx == 1 => continue,
+                'v' if state.dx == -1 => continue,
+                _ => {},
+            }
+            neighbors.push(State { cost, x, y, dx, dy, path_id: state.path_id + dpath});
+            dpath += 1;
+        }
+        for nstate in neighbors {
+            if visited.contains(&nstate) {
+                continue;
+            }
+            visited.insert(nstate.clone());
+            queue.push_back(nstate);
             dist_grid[x as usize][y as usize] = cost;
         }
+//      util::grid::print_gridvec(&dist_grid, 5, 0);
+//      util::debug::wait_for(10);
     }
-    util::grid::print_gridvec(&dist_grid, 5, 0);
-    max_distance
+    max_distances.values().max().unwrap().clone()
+}
+
+fn longest_path(grid: &mut [&mut [char]]) -> u32 {
+    let size = grid.len();
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+    queue.push_back(State {x:0, y:1, dx:1, dy:0, cost: 0, path_id: 0});
+
+    let mut dist_grid: Vec<Vec<u32>> = vec![vec![0; size]; size];
+
+    fin_path(grid, &mut queue, &mut visited, &mut dist_grid)
 }
 
 
